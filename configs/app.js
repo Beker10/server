956fd00b7e0c32a95b9b93ae1de754a67f01d73b
Beker Diaz <bekerdiaz668@gmail.com>
2026-05-19 15:29:44 -0600

    deploy new

--- configs/app.js
+++ configs/app.js
@@ -23,7 +23,7 @@ const middlewares = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }))
     app.use(cors({
-        origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+        origin: '*',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization']
