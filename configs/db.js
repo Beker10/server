bdeafc6330d068bb025a48fa5daa0c8d0ebdea17
Beker Diaz <bekerdiaz668@gmail.com>
2026-05-20 15:28:14 -0600

    fix: connection db

--- configs/db.js
+++ configs/db.js
@@ -3,6 +3,9 @@
 import mongoose from 'mongoose'
 
 export const dbConnection = async () => {
+    if (mongoose.connection.readyState === 1 || mongoose.connection.readyState === 2) {
+        return;
+    }
     try {
         console.log("hola")
         mongoose.connection.on('error', () => {
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import { initServer, app } from './configs/app.js';
+import { dbConnection } from './configs/db.js';
 
 // For local development
 if (process.env.VERCEL !== '1') {
     initServer();
+} else {
+    await dbConnection();
 }
 
 // Export for Vercel
 export default app;
-// force restart nodemon
